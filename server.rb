7cfc15954ff22a5acfaa3617f74bacca1623befd
Joel D'Autilio <joeldautilio@gmail.com>
2022-04-26 21:15:49 -0300

    Configure reloader

--- server.rb
+++ server.rb
@@ -1,11 +1,21 @@
 require 'sinatra/base'
+require 'bundler/setup'
+
+require 'sinatra/reloader' if Sinatra::Base.environment == :development
 
 class App < Sinatra::Application
-    def initialize(app = null)
+    configure :development do
+        register Sinatra::Reloader
+        after_reload do
+            puts 'Reloaded...'
+        end
+    end
+
+    def initialize(app = nil)
         super()
     end
 
     get '/' do
-        'Welcome'
+        'Welcome to the Internet'
     end
 end
