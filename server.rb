5fe739b4d200a26fc08ab6c47d8be5ae341bb8fd
Joel D'Autilio <joeldautilio@gmail.com>
2022-04-28 10:46:27 -0300

    login test

--- server.rb
+++ server.rb
@@ -3,7 +3,16 @@ require 'bundler/setup'
 
 require 'sinatra/reloader' if Sinatra::Base.environment == :development
 
+class User
+    def initialize(username,password)
+        @username = username
+        @password = password
+    end
+    attr_accessor :username,:password
+end
+
 class App < Sinatra::Application
+
     configure :development do
         register Sinatra::Reloader
         after_reload do
@@ -11,11 +20,28 @@ class App < Sinatra::Application
         end
     end
 
+    configure do
+        set :views, 'views'
+    end
+
     def initialize(app = nil)
         super()
     end
 
     get '/' do
-        'Welcome to the Internet'
+      'Hello World!'
     end
+    
+    users = [User.new("joel","1234")];
+
+    post '/login' do
+        users.to_s
+        # usr = User.new(params['username'],params['password'])
+        # users.push(usr)
+    end
+
+    get '/login' do
+        erb :login
+    end
+
 end
