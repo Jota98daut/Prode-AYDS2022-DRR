b535b22eaade525494f5e6702483d26dabae1231
Joel D'Autilio <joeldautilio@gmail.com>
2022-05-14 18:29:16 -0300

    Implements points method for score

--- models/score.rb
+++ models/score.rb
@@ -1,4 +1,11 @@
 class Score < ActiveRecord::Base
   belongs_to :player
   belongs_to :tournament
+
+  def points
+    acum = 0
+    bets = player.bets.select { |bet| bet.match.stage.tournament == tournament }
+    bets.each { |bet| acum = acum + bet.points }
+    return acum
+  end
 end
