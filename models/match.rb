6b3b4b3e945d7f954f1acadbed57805d18d49530
PabloRoss7 <pablorossi2001@gmail.com>
2022-05-15 17:45:23 -0300

    match callback

--- models/match.rb
+++ models/match.rb
@@ -8,4 +8,19 @@ class Match < ActiveRecord::Base
     validates :home, presence: true
     validates :away , presence: true
     validates :stage , presence: true
+
+    before_update :calculate_points, if: :result_charged?
+
+    def result_charged? 
+        return draw.present? || winner.present?
+    end
+
+    private
+      def calculate_points 
+        t = stage.tournament
+        bets.each do |bet|  
+            player_score = bet.player.scores.select {|score| score.tournament == t}
+            player_score.points = player_score.points + bet.points
+        end
+      end
 end
