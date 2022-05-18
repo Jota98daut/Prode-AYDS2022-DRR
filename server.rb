d67041c25e20f847181012770b88ea1d5046820a
Joel D'Autilio <joeldautilio@gmail.com>
2022-05-18 14:05:08 -0300

    Add session logic
    When a post is received, check whether the user exists. If it does,
    check the password. On success, set session user and redirect.
    Otherwise, prompt form again.

--- server.rb
+++ server.rb
@@ -8,50 +8,54 @@ require_relative 'models/init'
 
 class App < Sinatra::Application
 
-    configure :development do
-        register Sinatra::Reloader
-        after_reload do
-            puts 'Reloaded...'
-        end
+  configure :development do
+    register Sinatra::Reloader
+    after_reload do
+      puts 'Reloaded...'
     end
+  end
 
-    configure do
-        enable :sessions
-        # set :sessions, true
-        set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
-        set :views, 'views'
+  configure do
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+    set :views, 'views'
+  end
+
+  def initialize(app = nil)
+    super()
+  end
+
+  get '/' do
+    if session[:user_id]
+      @current_user = User.find_by(id: session[:user_id])
+      "Username: #{@current_user.username}\nPassword digest: #{@current_user.password_digest}"
+    else
+      redirect '/login'
     end
+  end
 
-    def initialize(app = nil)
-        super()
-    end
+  get '/signup' do
+    '<h1>Sign Up</h1>
+     <p>Not yet implemented</p>'
+  end
 
-    get '/' do
-      if session[:user_id]
-        @current_user = User.find_by(id: session[:user_id])
-        "Hello, #{@current_user.username}"
-      else
-        redirect '/login'
-      end
-    end
-
-    get '/signup' do
-      '<h1>Sign Up</h1>
-       <p>Not yet implemented</p>'
-    end
-
-    post '/login' do
-      user = User.find_by username: params['username']
-      if user
+  post '/login' do
+    user = User.find_by( username: params['username'] )
+    if user     # if there is an user with that username
+      user = user.authenticate( params['password'] )
+      if user   # and the password is correct
         session[:user_id] = user.id
         redirect '/'
       else
-        redirect '/signup'
+        redirect '/login'
       end
+    else
+      redirect 'login'
     end
+  end
 
-    get '/login' do
-        erb :login
-    end
+  get '/login' do
+    erb :login
+  end
 
 end
