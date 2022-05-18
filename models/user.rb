b7942bd0d8065f14329f70e331e5e8064df98268
Joel D'Autilio <joeldautilio@gmail.com>
2022-05-18 09:50:56 -0300

    Add Admin user type

--- models/user.rb
+++ models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
+    has_secure_password
 end
 
 class Player < User
     has_many :championbets
     has_many :bets
     has_many :scores
+end
+
+class Admin < User
 end
