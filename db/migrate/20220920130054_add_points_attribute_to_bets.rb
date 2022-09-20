2ca2a05e01280d09695fc3b088e64d3be25e971b
PabloRoss7 <pablorossi2001@gmail.com>
2022-09-20 13:26:06 -0300

    Fix add points to bet migration

--- db/migrate/20220920130054_add_points_attribute_to_bets.rb
+++ db/migrate/20220920130054_add_points_attribute_to_bets.rb
@@ -1,5 +1,5 @@
 class AddPointsAttributeToBets < ActiveRecord::Migration[7.0]
   def change
-    add_column :bets, :points, :int, default: 0, null: true
+    add_column :bets, :points, :int, default: nil, null: true
   end
 end
