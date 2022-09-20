67d4de299137b104ed47ee786b9f7559a98e1ce3
Joel D'Autilio <joeldautilio@gmail.com>
2022-09-20 11:34:57 -0300

    Order ranking by points

--- helpers/ranking_helper.rb
+++ helpers/ranking_helper.rb
@@ -2,7 +2,7 @@ module RankingHelper
 
     def get_rankings
         @tournament = Tournament.find_by id: params[:tournament_id]
-        @scores = Score.order(:points, :desc).where(tournament: @tournament)
+        @scores = Score.order(points: :desc).where(tournament: @tournament)
         erb :rankings
     end
 
